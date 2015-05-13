cdfc384ad1387d47543b0c8ecb0bd0e646f4120b
unknown <21204416@208-11.ens.fsi>
2015-05-13 17:06:10 +0200

    - Modification du script de création des tables de la BDD. - Création de la classe java de connexion Connect.

---
+++ maven_java_project/src/main/java/com/mycompany/maven_java_project/Connect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maven_java_project;
+
+import java.sql.*;
+
+/**
+ *
+ * @author 21204416
+ */
+public class Connect {
+    public static void main (String[] args)
+    {
+        try {
+          Class.forName("org.postgresql.Driver");
+          System.out.println("Driver O.K.");
+
+          String url = "jdbc:postgresql://localhost:5432/AppliReseau";
+          String user = "postgres";
+          String passwd = "postgres";
+
+          Connection conn = DriverManager.getConnection(url, user, passwd);
+          System.out.println("Connexion effective !");         
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+    }
+}
